8770bebf1afbe2dc919617dc4d92e59ff9a98a15
Jörg Bächtiger <joerg.baechtiger@finnova.ch>
2016-09-29 11:13:49 +0200

    no message

--- src/main/scala/eightyDays/scala211/bank/Model.scala
+++ src/main/scala/eightyDays/scala211/bank/Model.scala
@@ -3,12 +3,16 @@ package eightyDays.scala211.bank
 import eightyDays.scala211.bank.partner.{Identification, Partner}
 
 package partner {
-  case class Identification(number:String = java.util.UUID.randomUUID.toString)
-  abstract class Partner(val name:String)
-  case class Person(firstName:String, override val name: String) extends Partner(name)
+
+  case class Identification(number: String = java.util.UUID.randomUUID.toString)
+
+  abstract class Partner(val name: String)
+
+  case class Person(firstName: String, override val name: String) extends Partner(name)
+
 }
 
-case class Bank(name:String, partners: Map[Identification, Partner] = Map[Identification, Partner]()) {
+case class Bank(name: String, partners: Map[Identification, Partner] = Map[Identification, Partner]()) {
 
   def searchPartners(name: String) =
     partners
@@ -16,12 +20,12 @@ case class Bank(name:String, partners: Map[Identification, Partner] = Map[Identi
       .values
       .toSet
 
-  def addPartner(pPartner: Partner) =
+  def addPartner(pPartner: Partner): (Identification, Bank) =
     partners
       .find(_._2 == pPartner)
-      .map{entry => (entry._1, this)}
-      .getOrElse{
+      .map { entry => (entry._1, this) }
+      .getOrElse {
         val result = Identification()
         (result, copy(partners = partners + (result -> pPartner)))
-    }
+      }
 }
