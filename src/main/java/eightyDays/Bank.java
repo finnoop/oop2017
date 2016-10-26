a698b40fc68a73be121ec85a1fb5f070c52e3c6a
Andreas Boss <andreas.boss@finnova.com>
2016-10-26 12:27:24 +0200

    added

--- src/main/java/eightyDays/Bank.java
+++ src/main/java/eightyDays/Bank.java
@@ -18,23 +18,4 @@ public class Bank {
     public Optional<Partner> getPartner(Identication id) {
         return Optional.of(partners.get(id));
     }
-
-    @Override
-    public boolean equals(Object pO) {
-        if (this == pO) return true;
-        if (!(pO instanceof Bank)) return false;
-
-        Bank bank = (Bank) pO;
-
-        if (name != null ? !name.equals(bank.name) : bank.name != null) return false;
-        return partners != null ? partners.equals(bank.partners) : bank.partners == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (partners != null ? partners.hashCode() : 0);
-        return result;
-    }
 }
