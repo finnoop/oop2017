eb797ed3f4f19870dd68649dc8abe436a361af5a
Jörg Bächtiger <joerg.baechtiger@finnova.ch>
2016-10-26 11:52:40 +0200

    no message

---
+++ steps/2/test/java/eightyDays/LegalEntityTest.java
@@ -0,0 +1,23 @@
+package eightyDays;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LegalEntityTest {
+    @Test
+    public void getForm() throws Exception {
+        // TBD
+    }
+
+    @Test
+    public void equals() throws Exception {
+        // TBD
+    }
+
+    @Test
+    public void hashCode() throws Exception {
+        // TBD
+    }
+
+}
