a341ceafcff7023900f8b60bf10808f2e96eea85
Jörg Bächtiger <joerg.baechtiger@finnova.ch>
2016-11-04 23:23:38 +0100

    no message

--- src/test/scala/eightyDays/scala/package.scala
+++ src/test/scala/eightyDays/scala/package.scala
@@ -1,8 +1,5 @@
 package eightyDays
 
-/**
-  * Created by Andreas Boss on 06/10/16.
-  */
 package object scala {
   val phileasFogg = Person("Phileas", "Fogg")
   val jeanPassepartout = Person("Jean", "Passepartout")
